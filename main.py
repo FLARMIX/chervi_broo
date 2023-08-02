b5b09e90f193f9a134b8c0abeadca3ebaccd53a6
ASUS <FLARMIX@yandex.ru>
2023-08-03 02:58:07 +0400

    fix

--- main.py
+++ main.py
@@ -8,53 +8,6 @@ client = Client('telepython', api_id, api_hash)
 
 print('Script started!')
 
-openai.api_key = "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"
-
-messages = [
-    {"role": "system",
-     "content": "you answer short and clear, if you are asked to give a short answer you will make the answer as short as possible"},
-]
-
-
-def update(messages, role, content):
-    messages.append({"role": role, "content": content})
-    return messages
-
-
-def get_response(messages):
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-    return response['choices'][0]['message']['content']
-
-
-@client.on_message(filters.regex('prompt'), filters.me)
-def ai_text(client, message):
-    global messages
-    iteration = 0
-    for i in client.get_chat_history(message.chat.id):
-        iteration += 1
-        if iteration == 1:
-            with open('data.json', 'w', encoding='utf-8') as file:
-                file.write(str(i))
-            break
-
-    with open('data.json', 'r', encoding='utf-8') as file:
-        last_message = json.loads(file.read())
-
-    text = re.sub('prompt ', '', last_message.get('text'))
-
-    try:
-        user_input = text + ' дай максимально короткий ответ'
-
-        messages = update(messages, "user", user_input)
-
-        model_response = get_response(messages)
-
-        client.send_message(message.chat.id, model_response)
-
-        messages = update(messages, "assistant", model_response)
-
-    except:
-        client.send_message(message.chat.id, 'Не удалось обработать запрос')
 
 
 @client.on_message(filters.regex('help') & filters.me)
